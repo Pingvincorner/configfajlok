5e82e2a3d166fdb79fe3a5d9b6eeec0c62b31e79
SzzS <info@szarkazsolt.hu>
2021-06-18 11:47:03 +0200

    A a kiválasztott csoport a jelenlegi, akkor nem vált át az előzőleg használtra.

--- qtile/SzzS/desktop/config.py
+++ qtile/SzzS/desktop/config.py
@@ -207,7 +207,7 @@ groups = [
 for i in groups:
     keys.extend([
         # mod1 + letter of group = switch to group
-        Key([mod], i.name, lazy.group[i.name].toscreen(), desc="Switch to group {}".format(i.name)),
+        Key([mod], i.name, lazy.group[i.name].toscreen(toggle=False), desc="Switch to group {}".format(i.name)),
 
         # mod1 + shift + letter of group = switch to & move focused window to group
         Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
